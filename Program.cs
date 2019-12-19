b5a392186e0c63e8d65a715ff072ccbdd5ef57b6
Convidado Ventures <convidado@Wventures.interno>
2019-12-19 14:46:17 -0300

    Nova Vers

--- Program.cs
+++ Program.cs
@@ -7,16 +7,17 @@ namespace Ex2
         static void Main(string[] args)
         {
             // Void que vai rodar;
-            Ex1();
-            Ex2();
-            Ex3();
-            Ex4();
-            Ex5();
+            MediaIdade();
+            ImparPar();
+            Maioridade();
+            Salario();
+            Altura();
+            Fibbinacci();
+            Palindrome();
         }
 
-        static void Ex1()
+        static void MediaIdade()
         {
-
             Console.WriteLine("Digite a idade dos 5 alunos: ");
 
             int input1 = Convert.ToInt32(Console.ReadLine ());
@@ -29,23 +30,19 @@ namespace Ex2
 
             Console.WriteLine("A média das idades é: {0}",idadem);
         }
-        static void Ex2()
+        static void ImparPar()
         {
-
             Console.WriteLine("Digite um número: ");
             int input = Convert.ToInt32(Console.ReadLine ());
 
             if (input%2 == 0)
-            {
                 Console.WriteLine("O número é par.");
-            }else{
+            else
                 Console.WriteLine("O número é ímpar.");
-            }
 
         }
-        static void Ex3()
+        static void Maioridade()
         {
-
             Console.WriteLine("Digite a idade das 10 pessoas: ");
 
             int input1 = Convert.ToInt32(Console.ReadLine ());
@@ -65,16 +62,13 @@ namespace Ex2
             foreach (int input in inputs)
             {
                 if (input >= 18)
-                {
                     maiores++;
-                }
             }
 
             Console.WriteLine("O número de maiores de idade é: {0}",maiores);
         }
-        static void Ex4()
+        static void Salario()
         {
-
             Console.WriteLine("Digite o salário dos 10 funcionários: ");
 
             double sal1 = Convert.ToDouble(Console.ReadLine ());
@@ -108,9 +102,8 @@ namespace Ex2
                 }
             }
         }
-        static void Ex5()
+        static void Altura()
         {
-
             Console.WriteLine("Digite a altura dos 10 alunos: ");
 
             double input1 = Convert.ToDouble(Console.ReadLine ());
@@ -127,6 +120,43 @@ namespace Ex2
             double [] alturas = new double [] {input1,input2,input3,input4,input5,input6,input7,input8,input9,input10};
             Array.Sort(alturas);
             Console.WriteLine("A altura mais baixa é {0}, e a mais alta é {1}",alturas[0],alturas[9]);
+        }
+        static void Fibbinacci()
+        {
+            long n = 2, a1 = 1, a2 = 1;
+            Console.WriteLine(a1);
+            Console.WriteLine(a2);
+
+            while (n <= 100)
+            {
+                a1=a1+a2;
+                Console.WriteLine(a1);
+                a2=a1+a2;
+                Console.WriteLine(a2);
+                n = n + 2;
+            }
+            Console.WriteLine("100 primeiros números da sequência de Fibonnacci");
+
+        }
+        static void Palindrome()
+        {
+            Console.WriteLine("Escreva uma palavra");
+            string word = Console.ReadLine();
+            char[] seq = word.ToCharArray();
+            
+            bool isPalindrome = true;
+
+            for (int i = 0; i <= seq.Length/2; i++)
+            {
+                if (seq[i] != seq[seq.Length - 1 - i])
+                    isPalindrome = false;
+            }
+
+            if (isPalindrome)
+                Console.WriteLine("A palavra é um palíndromo");
+            else
+                Console.WriteLine("A palavra não é um palíndromo");
+
         }
     }
 }
